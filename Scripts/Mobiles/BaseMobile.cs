872245204b9031b2e8dbe12629389581cdd1429c
Merix <miguelamerida.89@gmail.com>
2020-10-12 12:39:03 +0200

    [Bugfix] Prevent to pick items if the BaseMobile is IsOverloaded. Only can move items inside their backpack or parpedoll

--- Scripts/Mobiles/BaseMobile.cs
+++ Scripts/Mobiles/BaseMobile.cs
@@ -1,3 +1,5 @@
+using Server.Misc;
+
 namespace Server.Mobiles
 {
 	public abstract partial class BaseMobile : Mobile
@@ -8,6 +10,21 @@ namespace Server.Mobiles
 
 		public BaseMobile(Serial serial) : base(serial)
 		{
+		}
+
+		public override bool OnDragLift(Item item)
+		{
+			//Only check if the item don't have parent or the parent is different to the player mobile
+			if (item.Parent == null || (item.Parent != null && item.RootParent != Backpack && item.RootParent != this))
+			{
+				if (WeightOverloading.IsOverloaded(this))
+				{
+					SendMessage(0x22, "You are too heavy to carry more weight.");
+					return false;
+				}
+			}
+
+			return base.OnDragLift(item);
 		}
 
 		public override void Serialize(GenericWriter writer)
