42790520c6e61462dc7f1597e0e6392998635940
Merix <miguelamerida.89@gmail.com>
2020-10-12 11:54:16 +0200

    Change FoodDecay system to work with two differents type. Global - 1 Timer and food decay every X minutes Individual - 1 timer each playermobile and run every X minutes

--- Scripts/Misc/FoodDecay.cs
+++ Scripts/Misc/FoodDecay.cs
@@ -1,23 +1,93 @@
-using System;
+using Server.Mobiles;
 using Server.Network;
+using System;
+using System.Collections.Generic;
 
 namespace Server.Misc
 {
+	public enum FoodDecayType
+	{
+		Global = 0,
+		Individual = 1
+	}
+
 	public class FoodDecayTimer : Timer
 	{
-		public static void Initialize()
+		public static bool Enabled => Settings.Get<bool>("FoodDecay", "Enabled");
+		public static float DecayDelay => Settings.Get<float>("FoodDecay", "DecayDelay");
+		public static FoodDecayType TimerType => (FoodDecayType)Settings.Get<int>("FoodDecay", "DecayType");
+
+		private static Dictionary<Mobile, Timer> m_Timers = new Dictionary<Mobile, Timer>();
+
+		public static Dictionary<Mobile, Timer> Timers
 		{
-			new FoodDecayTimer().Start();
+			get { return m_Timers; }
 		}
 
-		public FoodDecayTimer() : base(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(5))
+		public static void Initialize()
 		{
+			if (TimerType == FoodDecayType.Individual)
+			{
+				EventSink.Login += EventSink_Login;
+				EventSink.Logout += EventSink_Logout;
+			}
+			else
+			{
+				new FoodDecayTimer().Start();
+			}
+		}
+
+		private static void EventSink_Login(LoginEventArgs e)
+		{
+			if (e.Mobile != null && e.Mobile is PlayerMobile pm)
+			{
+				CreateTimer(pm);
+			}
+		}
+
+		private static void EventSink_Logout(LogoutEventArgs e)
+		{
+			if (e.Mobile != null)
+			{
+				StopTimer(e.Mobile);
+			}
+		}
+
+		private readonly Mobile m_Mobile;
+
+		public FoodDecayTimer() : base(TimeSpan.FromMinutes(DecayDelay), TimeSpan.FromMinutes(DecayDelay))
+		{
+			Priority = TimerPriority.OneMinute;
+		}
+
+		public FoodDecayTimer(Mobile mob) : base(TimeSpan.FromMinutes(DecayDelay), TimeSpan.FromMinutes(DecayDelay))
+		{
+			m_Mobile = mob;
 			Priority = TimerPriority.OneMinute;
 		}
 
 		protected override void OnTick()
 		{
-			FoodDecay();
+			switch (TimerType)
+			{
+				case FoodDecayType.Global:
+					FoodDecay();
+					break;
+				case FoodDecayType.Individual:
+					if (m_Mobile != null)
+					{
+						HungerDecay(m_Mobile);
+						ThirstDecay(m_Mobile);
+					}
+					else
+					{
+						//If mobile is null, stop the timer to delete it
+						Stop();
+					}
+					break;
+				default:
+					break;
+			}
 		}
 
 		public static void FoodDecay()
@@ -39,6 +109,29 @@ namespace Server.Misc
 		{
 			if (m != null && m.Thirst >= 1)
 				m.Thirst -= 1;
+		}
+
+		public static void CreateTimer(Mobile mob)
+		{
+			if (mob != null)
+			{
+				if (!m_Timers.ContainsKey(mob))
+				{
+					m_Timers[mob] = new FoodDecayTimer(mob);
+					m_Timers[mob].Start();
+				}
+			}
+		}
+
+		public static bool StopTimer(Mobile m)
+		{
+			if (m_Timers.TryGetValue(m, out Timer timer))
+			{
+				timer.Stop();
+				m_Timers.Remove(m);
+			}
+
+			return (timer != null);
 		}
 	}
 }
